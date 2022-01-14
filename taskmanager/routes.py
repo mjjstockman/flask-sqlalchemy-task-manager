cecbd73eae77a7be8e70cb490f52d33e384b9b04
mjjstockman <mjjstockman@gmail.com>
2022-01-14 13:49:15 +0000

    Add form to add new tasks

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -53,3 +53,29 @@ def delete_category(category_id):
     db.session.delete(category)
     db.session.commit()
     return redirect(url_for("categories"))
+
+
+@app.route("/add_task", methods=["GET", "POST"])
+def add_task():
+    # query the table using this category_name to get all cataogory names
+    #  from db, store in var called category
+    categories = list(Category.query.order_by(Category.category_name).all())
+    # can check models to see task schema (ie. needs name, descrition etc)
+    if request.method == "POST":
+        # using POST method as form info is being sent
+        # create task var which hold all the info sent from the form fields
+        task = Task(
+            task_name=request.form.get("task_name"),
+            task_description=request.form.get("task_description"),
+            is_urgent=bool(True if request.form.get("is_urgent") else False),
+            due_date=request.form.get("due_date"),
+            category_id=request.form.get("category_id")
+        )
+        # add the data to the session
+        db.session.add(task)
+        # commit the data to the db
+        db.session.commit()
+        # if all good, return user to home page
+        return redirect(url_for("home"))
+    # if method not POST, show the add_task page and send the categories as a param
+    return render_template("add_task.html", categories=categories)
