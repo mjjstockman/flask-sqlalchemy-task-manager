9f8d6ba3a64a61e7aa43f6c22329aced45f380b2
mjjstockman <mjjstockman@gmail.com>
2022-01-16 09:41:14 +0000

    Add delete functionality to tasks

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -5,6 +5,9 @@ class Category(db.Model):
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
+    # use backref and cascade to denote relationships between the tables
+    # below will delete rows from both tables when one uses PK as FK and keep
+    # id numbers in table will reflect deletion
     tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
